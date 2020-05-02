dfee91f2b45e0570be5b7913ca6f0fe83ca376e1
Nicholas Mullen <nwmullen@gmail.com>
2020-05-01 22:08:59 -0400

    mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,55 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
 import Login from "./components/Login";
+import BubblePage from './components/BubblePage';
 import "./styles.scss";
 
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      localStorage.getItem("token") ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/" />
+      )
+    }
+  />
+);
+
 function App() {
+  const [loggedIn, setLoggedIn] = useState(localStorage.getItem('token'));
+
+  function loginCallback() {
+    setLoggedIn(true);
+  }
+
+  function logout(event) {
+    event.preventDefault();
+    localStorage.removeItem('token');
+    setLoggedIn(false);
+  }
+
   return (
     <Router>
+      <h1>Welcome to the Bubble App!</h1>
+      {loggedIn && (
+        <>
+          <div>
+            <a href="" onClick={logout}>Logout</a>
+          </div>
+          <br/>
+        </>
+      )}
       <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        {/*<Route exact path="/" component={Login} />*/}
+        <Route
+          exact
+          path="/"
+          render={(props)=><Login {...props} loginCallback={loginCallback} />}
+        />
+        <PrivateRoute path='/bubbles' component={BubblePage} />
       </div>
     </Router>
   );
--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from '../axiosAuth';
 
 const initialColor = {
   color: "",
@@ -7,7 +7,6 @@ const initialColor = {
 };
 
 const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
@@ -18,12 +17,26 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
+    axiosWithAuth().put(`/colors/${colorToEdit.id}`, colorToEdit).then(res => {
+      setEditing(false);
+      updateColors(colors.map(color => {
+        if (color.id === colorToEdit.id) {
+          return res.data;
+        } else {
+          return color;
+        }
+      }));
+    });
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
   };
 
   const deleteColor = color => {
+    axiosWithAuth().delete(`/colors/${color.id}`).then(res => {
+      console.log('deleteResponse: ', res);
+      updateColors(colors.filter(loopColor => color.id !== loopColor.id));
+    });
     // make a delete request to delete this color
   };
 
